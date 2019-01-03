2a977188d6ce207cc90ddaefd90b29fdf62adac1
zhaohaoren <zhrlova@hotmail.com>
2019-01-03 21:00:20 +0800

    update

---
+++ mybatis/src/main/java/top/justin/ch04/model/CompanyFinancing.java
@@ -0,0 +1,17 @@
+package top.justin.ch04.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+public class CompanyFinancing {
+    private Date investDate;
+    private String compId;
+    private String comp_name;
+    private BigDecimal invest_money;
+    private String stage_name;
+    private Integer stage_id;
+    private Integer invest_cur_id;
+    private String inst_ids;
+    private String inst_names;
+}
